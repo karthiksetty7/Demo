6e39df503b7dc494bb634faf89c8ee6863c38e23
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-08 18:24:40 +0530

    Update server.js

--- rent-management-backend/server.js
+++ rent-management-backend/server.js
@@ -3,15 +3,16 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
 import buildingRoutes from './routes/buildingRoutes.js';
+import floorRoutes from './routes/floorRoutes.js'; // <- floor routes
 import { connectDB, sequelize } from './config/db.js';
 
 dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "*", // allow everything
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: ["null", "http://localhost:3000"], 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
 }));
 
 app.use(express.json());
@@ -19,11 +20,12 @@ app.get('/', (req, res) => res.send('API is running...'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/buildings', buildingRoutes);
+app.use('/api/floors', floorRoutes); // <--- updated
 
 const startServer = async () => {
   try {
     await connectDB();
-    await sequelize.sync(); // create tables if not exist
+    await sequelize.sync();
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
   } catch (err) {
