c4e1d992d27ef4e4ae652ce4c0a27161d1d054ac
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-08 16:48:36 +0530

    Update buildingRoutes.js

--- rent-management-backend/routes/buildingRoutes.js
+++ rent-management-backend/routes/buildingRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { getBuildings, addBuilding, deleteBuilding } from '../controllers/buildingController.js';
+import { getBuildings, addBuilding, deleteBuilding,updateBuilding } from '../controllers/buildingController.js';
 
 const router = express.Router();
 router.use(protect); // ALL building routes require auth
@@ -8,5 +8,6 @@ router.use(protect); // ALL building routes require auth
 router.get('/getBuildings', getBuildings);
 router.post('/addBuilding', addBuilding);
 router.delete('/deleteBuilding/:id', deleteBuilding);
+router.put('/updateBuilding/:id', updateBuilding);
 
 export default router;
