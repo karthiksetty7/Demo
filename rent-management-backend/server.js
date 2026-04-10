99a1ef0cba459c0e4bd431a3b43e8c3339f5f714
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-10 12:07:27 +0530

    Update server.js

--- rent-management-backend/server.js
+++ rent-management-backend/server.js
@@ -25,13 +25,18 @@ console.log("🔥 NEW SERVER DEPLOYED - CORS SHOULD WORK");
 // 1. BULLETPROOF CORS FIX
 // =========================
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  const origin = req.headers.origin;
+
+  if (origin === "http://localhost:3000") {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
-  // Handle preflight request
   if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
+    return res.sendStatus(204);
   }
 
   next();
