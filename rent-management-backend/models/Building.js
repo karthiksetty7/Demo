eb5688cbc571d8d603d241043e241626c1417c41
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-04-11 12:22:04 +0530

    Update Building.js

--- rent-management-backend/models/Building.js
+++ rent-management-backend/models/Building.js
@@ -2,8 +2,21 @@ import { DataTypes } from 'sequelize';
 import { sequelize } from '../config/db.js';
 
 const Building = sequelize.define('Building', {
-  name: { type: DataTypes.STRING, allowNull: false },
-  address: { type: DataTypes.STRING, allowNull: false },
+  name: { 
+    type: DataTypes.STRING, 
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  address: { 
+    type: DataTypes.STRING, 
+    allowNull: false,
+    unique: true,   // uniqueness
+    validate: {
+      notEmpty: true
+    }
+  },
 });
 
 export default Building;
